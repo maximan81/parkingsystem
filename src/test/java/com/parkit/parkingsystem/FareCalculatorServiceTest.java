b3997104538a888493d1a0128e38f9ec3ba83c0d
Eric Maximan <ericmaximan@gmail.com>
2022-05-25 10:10:50 +0200

    update test class FareCalculatorServiceTest to add free option test

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -153,6 +153,22 @@ public class FareCalculatorServiceTest {
     }
 
 
+    @Test
+    public void givenFreeFareBikeWithLessThirtyOneMinutesParkingTime() {
+        // GIVEN
+        Date inTime = new Date();
+        inTime.setTime(System.currentTimeMillis() - (30 * 60 * 1000));
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
 
+        // WHEN
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        fareCalculatorService.calculateFare(ticket);
+
+        // THEN
+        assertEquals((0.0 * Fare.CAR_RATE_PER_HOUR), ticket.getPrice());
+    }
 
 }
